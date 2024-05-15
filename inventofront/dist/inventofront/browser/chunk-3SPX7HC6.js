8934c72c0fe326ef18fa8915d08690ad86a7fe57
Vaibhav Makhloga <vaibhav.makhloga@alfalaval.com>
2024-05-15 12:39:58 +0530

    changed signal version

---
+++ inventofront/dist/inventofront/browser/chunk-3SPX7HC6.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-X6YHEMIY.js";import"./chunk-UX26MTZT.js";export{a as BrowserPerformanceMeasurement};
---
+++ inventofront/dist/inventofront/browser/chunk-ZK34JXPM.js
@@ -0,0 +1,1 @@
+import{f as a}from"./chunk-N5D56HDE.js";import"./chunk-FY4HQCBD.js";import"./chunk-UX26MTZT.js";export{a as StandardController};
